31f632f5d6df405e0a60f24ee0e8f60eb1d9d283
Chris Parnin <chris.parnin@gmail.com>
2017-06-11 01:28:10 -0400

    Working mustache and prompting test.

--- src/test/mustache.js
+++ src/test/mustache.js
@@ -1,16 +1,61 @@
 const mustache = require('mustache');
 const yaml = require('js-yaml');
 const fs = require('fs');
+const prompt = require("prompt");
 
-var view = {
-  title: "Joe",
-  calc: function() {
-    return 2 + 4;
-  }
-};
+async function promptValue(propertyName, description)
+{
+    return new Promise((resolve, reject) => {
+        prompt.start();
+        prompt.get([{name: propertyName, description: description }], function (err, result)
+        {
+            if (err) { console.log(err); }
+            //prompt.stop();
+            resolve(result[propertyName]);
+        });
+    });
+}
 
-template = fs.readFileSync( "../config/BaseVM.mustache" ).toString();
-let doc = yaml.safeLoad(fs.readFileSync("resources/baker.yml", 'utf8'));
+async function traverse(o) {
+    const stack = [{obj: o, parent: null, parentKey:""}]
 
-var output = mustache.render(template, doc);
-console.log(output);
+    while (stack.length) {
+        const s = stack.shift()
+        const obj = s.obj;
+        const parent = s.parent;
+        const parentKey = s.parentKey;
+
+        for( var i = 0; i < Object.keys(obj).length; i++ )
+        {
+            let key = Object.keys(obj)[i];
+
+            //await fn(key, obj[key], obj)
+
+            if (obj[key] instanceof Object) {
+                stack.unshift({obj: obj[key], parent: obj, parentKey: key})
+            }
+
+            if( key == "prompt")
+            {
+                const input = await promptValue(parentKey, obj[key]);
+                // Replace "prompt" with an value provided by user.
+                parent[parentKey] = input;
+            }
+
+        }
+    }
+    return o;
+}
+
+( async() =>
+{
+    template = fs.readFileSync( "../config/BaseVM.mustache" ).toString();
+    let doc = yaml.safeLoad(fs.readFileSync("resources/baker.yml", 'utf8'));
+
+    const vagrant = doc.vagrant;
+    const x = await traverse(vagrant);
+    const output = mustache.render(template, doc);
+
+    console.log(output);
+})();
+
