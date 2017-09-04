644b7dd0f8ca8e79dc5154b1390c4b407c502f29
Samim Mirhosseini <samim.mirhoseini@gmail.com>
2017-09-04 17:24:19 -0400

    Updated int-test to use new commands

--- src/test/integration/baker-test-repo.js
+++ src/test/integration/baker-test-repo.js
@@ -9,30 +9,28 @@ describe('Command line tests', function() {
 
 describe('baker should create VM and destroy it', function() {
     this.timeout(600000);
-    
-    it('should have running node server', function(done) 
-    {
-        var child = child_process.exec('node baker.js --local test/resources/baker-test', function(error, stdout, stderr){
+
+    it('should have running node server', function(done) {
+        var child = child_process.exec('node cmd.js bake --local test/resources/baker-test', function(error, stdout, stderr){
 
             var options = {
                 url: "http://localhost:3333/",
                 method: 'GET'
             };
-        
+
             // Send a http request to url and specify a callback that will be called upon its return.
-            request(options, function (error, response, body) 
+            request(options, function (error, response, body)
             {
                 expect(body).to.equal('Hi there!');
                 done();
             });
-        
+
         });
         child.stdout.pipe(process.stdout);
     });
 
-    it('should destroy VM', function(done) 
-    {
-        var child = child_process.exec('node baker.js --destroy baker-test', function(error, stdout, stderr)
+    it('should destroy VM', function(done) {
+        var child = child_process.exec('node cmd.js destroy baker-test', function(error, stdout, stderr)
         {
             console.log(stderr);
             expect(stderr).to.be.empty;
