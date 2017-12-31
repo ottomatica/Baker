299c204e912ec4602bd6318f9dce02e1d0ac38ca
Chris Parnin <chris.parnin@gmail.com>
2017-12-31 01:40:01 -0500

    Fix start command.

--- src/lib/bakerlets/start.js
+++ src/lib/bakerlets/start.js
@@ -4,8 +4,11 @@ const path = require('path');
 
 async function start(cmd,sshConfig)
 {
-    //await ssh.sshExec(`nohup ${cmd} &`, sshConfig, true);
-    await ssh.sshExec(`${cmd}`, sshConfig, true);
+    //await ssh.sshExec(`nohup bash -c '${cmd}' & exit 0`, sshConfig, true);
+    await ssh.sshExec(`nohup bash -c "${cmd}" > ~/start.out 2> ~/start.err &`, sshConfig, true);
+    // https://stackoverflow.com/questions/29142/getting-ssh-to-execute-a-command-in-the-background-on-target-machine
+    // can consider a keepalive option for start if want to see the output without redirecting.
+
 }
 
 module.exports = start;
