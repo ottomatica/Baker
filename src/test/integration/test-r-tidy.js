f417f1caa94904b023b4ef65de7b801ebd84c32f
Chris Parnin <chris.parnin@gmail.com>
2018-01-02 23:47:28 -0500

    test r-tidy

---
+++ src/test/integration/test-r-tidy.js
@@ -0,0 +1,29 @@
+const child_process = require('child_process');
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+const os = require('os');
+const path = require('path');
+const fs = require('fs-extra');
+
+describe('baker should create r tidy vm', function() {
+    this.timeout(2000000);
+
+    // https://github.ncsu.edu/engr-csc326-staff/Onboarding
+
+    it('should setup tidyverse', function(done) {
+        const tstDir = path.join(os.tmpdir(), 'r-tidy');
+        fs.mkdirpSync(tstDir);
+        fs.copySync('test/resources/baker2/r-tidy.yml', path.join(tstDir,'baker.yml'));
+        // echo value for prompt input for password.
+        var child = child_process.exec(`baker bake2 --local ${tstDir}`, 
+                                       {cwd: os.tmpdir()  }, function(error, stdout, stderr) 
+        {
+            console.log(stderr || stdout);
+            done();
+        });
+        child.stdout.pipe(process.stdout);
+    });
+});
+
+
