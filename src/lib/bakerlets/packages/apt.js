861a2461d3fd05939d40034b839b3a473062fc42
Samim Mirhosseini <samim.mirhoseini@gmail.com>
2018-01-08 11:42:40 -0500

    adding deb support for package Bakerlet

--- src/lib/bakerlets/packages/apt.js
+++ src/lib/bakerlets/packages/apt.js
@@ -17,22 +17,48 @@ class Apt extends Bakerlet {
 
     async load(obj, variables) {
         this.variables = variables;
+        this.packages = {default: [], deb: [], ppa: []};
 
+        // console.log('obj', obj.apt);
         if (obj.apt) {
             let type = typeof(obj.apt);
             if (type == 'string')
-                    this.packages = obj.apt.trim().split(/\s*,\s*/g);
-            else if(type = 'object')
-                    this.packages = obj.apt;
+                this.packages.default = obj.apt.trim().split(/\s*,\s*/g);
+            else if(type == 'object'){
+                obj.apt.forEach(pkg => {
+                    if(typeof(pkg) == 'object' && pkg[Object.keys(pkg)[0]].deb){
+                        let name = Object.keys(pkg)[0];
+                        this.packages.deb.push({name: name, deb: pkg[name].deb});
+                    }
+                    else if(typeof(pkg) == 'string'){
+                        this.packages.default.push(pkg);
+                    }
+
+                });
+            }
         }
 
-        let packagesObj = {'packages': this.packages.map(p => {return {'name': p}}) };
+        let packagesObj =   {
+                                'packages':
+                                    {
+                                        'default': this.packages.default.map(p => {return {'name': p}}),
+                                        'deb': this.packages.deb,
+                                        'ppa': this.packages.ppa.map(p => {return {'name': p}})
+                                    }
+                            };
+
+        // console.log('default', packagesObj.packages.default);
+        // console.log('deb', packagesObj.packages.deb);
+        // console.log('ppa', packagesObj.packages.ppa);
+        // console.log('packagesobj', packagesObj);
+
         let playbookTemplate = path.resolve(
             this.remotesPath,
             `bakerlets-source/packages/apt.yml.mustache`
         );
         let playbookRendered = mustache.render(await fs.readFileAsync(playbookTemplate, 'utf8'), packagesObj);
 
+        // console.log('playbookrendered', playbookRendered);
         let cmd = `echo "${playbookRendered.replace(/"/g, '\\"')}" > /home/vagrant/baker/${this.name}/apt.yml`;
         await this.exec(cmd);
     }
