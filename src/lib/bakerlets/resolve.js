1ac6be160064baf0996e4e320ffc5180a3923805
Samim Mirhosseini <samim.mirhoseini@gmail.com>
2018-02-13 16:07:34 -0500

    removing the extra vmSSHConfig

--- src/lib/bakerlets/resolve.js
+++ src/lib/bakerlets/resolve.js
@@ -33,7 +33,7 @@ module.exports.resolveBakerlet = async function(bakerletsPath, remotesPath, vmSS
         {
             for (var i = 0; i < doc.lang.length; i++)
             {
-                await resolve(doc.name, bakerScriptPath, remotesPath, path.join(bakerletsPath,"lang"), doc.lang[i], extra_vars, verbose, vmSSHConfig);
+                await resolve(doc.name, bakerScriptPath, remotesPath, path.join(bakerletsPath,"lang"), doc.lang[i], extra_vars, verbose);
             }
         }
 
@@ -41,7 +41,7 @@ module.exports.resolveBakerlet = async function(bakerletsPath, remotesPath, vmSS
         {
             for (var i = 0; i < doc.config.length; i++)
             {
-                await resolve(doc.name, bakerScriptPath, remotesPath, path.join(bakerletsPath,"config"), doc.config[i], extra_vars, verbose, vmSSHConfig);
+                await resolve(doc.name, bakerScriptPath, remotesPath, path.join(bakerletsPath,"config"), doc.config[i], extra_vars, verbose);
             }
         }
 
@@ -49,7 +49,7 @@ module.exports.resolveBakerlet = async function(bakerletsPath, remotesPath, vmSS
         {
             for (var i = 0; i < doc.services.length; i++)
             {
-                await resolve(doc.name, bakerScriptPath, remotesPath, path.join(bakerletsPath,"services"), doc.services[i], extra_vars, verbose, vmSSHConfig);
+                await resolve(doc.name, bakerScriptPath, remotesPath, path.join(bakerletsPath,"services"), doc.services[i], extra_vars, verbose);
             }
         }
 
@@ -57,7 +57,7 @@ module.exports.resolveBakerlet = async function(bakerletsPath, remotesPath, vmSS
         {
             for (var i = 0; i < doc.tools.length; i++)
             {
-                await resolve(doc.name, bakerScriptPath, remotesPath, path.join(bakerletsPath,"tools"), doc.tools[i], extra_vars, verbose, vmSSHConfig);
+                await resolve(doc.name, bakerScriptPath, remotesPath, path.join(bakerletsPath,"tools"), doc.tools[i], extra_vars, verbose);
             }
         }
 
@@ -65,14 +65,14 @@ module.exports.resolveBakerlet = async function(bakerletsPath, remotesPath, vmSS
         {
             for (var i = 0; i < doc.packages.length; i++)
             {
-                await resolve(doc.name, bakerScriptPath, remotesPath, path.join(bakerletsPath,"packages"), doc.packages[i], extra_vars, verbose, vmSSHConfig);
+                await resolve(doc.name, bakerScriptPath, remotesPath, path.join(bakerletsPath,"packages"), doc.packages[i], extra_vars, verbose);
             }
         }
 
         if( doc.env )
         {
             doc.env = [{env: doc.env}]; // fixing the format // TODO: it works ok, but probably too hacky
-            await resolve(doc.name, bakerScriptPath, remotesPath, path.join(bakerletsPath,"env"), doc.env[0], extra_vars, verbose, vmSSHConfig);
+            await resolve(doc.name, bakerScriptPath, remotesPath, path.join(bakerletsPath,"env"), doc.env[0], extra_vars, verbose);
         }
 
         if( doc.start )
