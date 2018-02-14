64aa130373e2137528adc2d17e54621a35f8f862
Samim Mirhosseini <samim.mirhoseini@gmail.com>
2018-02-13 19:03:06 -0500

    check if baker.yml already exists before running init

--- src/lib/modules/baker.js
+++ src/lib/modules/baker.js
@@ -23,7 +23,13 @@ module.exports = function(dep) {
     }
 
     result.initBaker2 = async function(){
-        const { fs, path, configPath, inquirer, validator, mustache } = dep;
+        const { fs, path, configPath, inquirer, validator, mustache, spinner, spinnerDot } = dep;
+
+        // TODO: Find a better approach to do this
+        try{
+            if(await fs.pathExists(await path.resolve(path.resolve(process.cwd(), 'baker.yml'))))
+                await spinner.spinPromise(Promise.reject(), `A baker.yml already exists in current directory!`, spinnerDot);
+        } catch (err) { return; }
 
         let vmResponse = await inquirer
             .prompt([
@@ -38,7 +44,7 @@ module.exports = function(dep) {
                     name: 'memory',
                     message: 'Amount of memory to share with this environment (in MB):',
                     choices: ['512', '1024', '2048', '3072', '4096'],
-                    default: '512'
+                    default: '1024'
                 },
                 {
                     type: 'input',
@@ -63,6 +69,9 @@ module.exports = function(dep) {
                     name: 'ports',
                     message: 'Forward ports comma separated, (GUEST:HOST) or (GUEST):',
                     validate: async function(value) {
+                        if(value === '')
+                            return true;
+
                         let ports = value.split(',').map(port => port.split(':'));
                         let invalidPorts = [];
 
@@ -126,11 +135,12 @@ module.exports = function(dep) {
                     ]
                 }
             ]);
-        vmResponse.langs = {lang: vmResponse.langs}
-        vmResponse.services = {service: vmResponse.services}
-        vmResponse.tools = {tool: vmResponse.tools}
 
-        // console.log(vmResponse)
+        // TODO: refactor
+        vmResponse.langs = vmResponse.langs.length ? {lang: vmResponse.langs} : false;
+        vmResponse.services = vmResponse.services.length ? {service: vmResponse.services} : false;
+        vmResponse.tools = vmResponse.tools.length ? {tool: vmResponse.tools} : false;
+
         let baker2Template = await fs.readFileAsync(path.join(configPath, './baker2Template.yml.mustache'), 'utf8');
         let bakerYML = mustache.render(baker2Template, vmResponse);
         let cwd = path.resolve(process.cwd());
