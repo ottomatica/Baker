eca8c55c925403745a0a942e047874a66499a650
Samim Mirhosseini <samim.mirhoseini@gmail.com>
2018-03-03 03:21:45 -0500

    @ssmirr Using info from cwd baker.yml by default. Commands: halt #48

--- src/lib/commands/halt.js
+++ src/lib/commands/halt.js
@@ -3,7 +3,7 @@
 module.exports = function(dep) {
     let cmd = {};
 
-    cmd.command = 'halt <VMName>';
+    cmd.command = 'halt [VMName]';
     cmd.desc = `shut down a VM`;
     cmd.builder = {
         force: {
@@ -16,9 +16,19 @@ module.exports = function(dep) {
 
     cmd.handler = async function(argv) {
         const { baker, print, spinner, spinnerDot } = dep;
-        const { VMName, force } = argv;
+        let { VMName, force } = argv;
 
         try {
+            if(!VMName){
+                let cwdVM = (await baker.getCWDBakerYML());
+                if(cwdVM)
+                    VMName = (await baker.getCWDBakerYML()).name;
+                else {
+                    print.error(`Couldn't find baker.yml in cwd. Run the command in a directory with baker.yml or specify a VMName.`);
+                    process.exit(1);
+                }
+            }
+
             await spinner.spinPromise(baker.haltVM(VMName, force), `Stopping VM: ${VMName}`, spinnerDot);
         } catch(err) {
             print.error(err);
