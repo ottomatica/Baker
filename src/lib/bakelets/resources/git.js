10ce9ac7540f7bd479bc386a03b3d5bd85ea948b
Christopher J. Parnin <chris.parnin@gmail.com>
2018-03-07 10:35:18 -0500

    Add private support.

--- src/lib/bakelets/resources/git.js
+++ src/lib/bakelets/resources/git.js
@@ -1,5 +1,7 @@
 const Bakelet  = require('../bakelet');
 const Baker    = require('../../modules/baker');
+const chalk = require('chalk');
+const _ = require('underscore');
 
 class Git extends Bakelet {
     constructor(name, ansibleSSHConfig, version) {
@@ -22,11 +24,25 @@ class Git extends Bakelet {
             else if(type == 'object'){
                 this.repo = obj.git.repo;
                 this.dest = obj.git.dest;
+                if( obj.git.private )
+                {
+                    if( this.variables.filter( x => x.githubuser ).length == 0 || this.variables.filter( x => x.githubpass ).length == 0 )
+                    {
+                        console.log(chalk.red("You must define a githubuser and githubpass variable in order to clone a private repo"));
+                        throw new Error("Cannot complete git operation.");
+                    }
+                    let user = encodeURIComponent(this.variables.filter( x => x.githubuser )[0].githubuser);
+                    let pass = encodeURIComponent(this.variables.filter( x => x.githubpass )[0].githubpass);
+                    // gitlab/bitbucket.
+                    this.repo = this.repo.replace('github.com', `${user}:${pass}@github.com`);
+                }
             }
         }
-
-        console.log('repo', this.repo);
-        console.log('dest', this.dest);
+        if( this.verbose )
+        {
+            console.log('repo', this.repo);
+            console.log('dest', this.dest);
+        }
     }
 
     async install() {
