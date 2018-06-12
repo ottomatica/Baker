68437806fedcd9780b4b6971816e902ce827a272
Samim Mirhosseini <samim.mirhoseini@gmail.com>
2018-06-12 16:35:34 -0400

    helper function to automatically use the right type of provider + updating bake command

--- src/lib/commands/bake.js
+++ src/lib/commands/bake.js
@@ -1,13 +1,10 @@
 const Baker          = require('../modules/baker');
 const conf           = require('../../lib/modules/configstore');
-const DockerProvider = require('../modules/providers/docker');
-const fs             = require('fs-extra');
 const Git            = require('../modules/utils/git');
 const path           = require('path');
 const Print          = require('../modules/print');
 const Spinner        = require('../modules/spinner');
-const VagrantProvider = require('../modules/providers/vagrant');
-const yaml           = require('js-yaml');
+const Utils          = require('../modules/utils/utils');
 
 const spinnerDot = conf.get('spinnerDot');
 
@@ -93,19 +90,7 @@ exports.handler = async function(argv) {
             }
         }
 
-        //let validation = await Spinner.spinPromise(Validator.validateBakerScript(bakePath), 'Validating baker.yml', spinnerDot);
-        let doc = yaml.safeLoad(await fs.readFile(path.join(bakePath, 'baker.yml'), 'utf8'));
-        let envType = doc.container ? 'container' : doc.vm || doc.vagrant ? 'vm' : 'other';
-
-        let provider = null;
-        if(envType === 'container')
-            provider = new DockerProvider({host: '192.168.252.251', port: '2375', protocol: 'http'});
-        else if(envType === 'vm')
-            provider = new VagrantProvider();
-        else
-            console.error('This command only supports VM and container environments');
-
-        let BakerObj = new Baker(provider);
+        const {provider, BakerObj} = await Utils.chooseProvider(bakePath);
 
         try
         {
