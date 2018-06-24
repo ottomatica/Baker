95023821d08332c7f9918bf64189043e8d56a4a5
Samim Mirhosseini <samim.mirhoseini@gmail.com>
2018-06-23 23:29:19 -0400

    adding latext bakelet

---
+++ src/lib/bakelets/tools/latex.js
@@ -0,0 +1,26 @@
+const Bakelet = require('../bakelet');
+const Baker   = require('../../modules/baker');
+const path    = require('path');
+
+class Latex extends Bakelet {
+
+    constructor(name, ansibleSSHConfig, version) {
+        super(ansibleSSHConfig);
+
+        this.name    = name;
+        this.version = version;
+    }
+
+    async load(obj, variables) {
+        let playbook = path.resolve(this.remotesPath, `bakelets-source/tools/latex/latex${this.version}.yml`);
+        await this.copy(playbook, `/home/vagrant/baker/${this.name}/latex${this.version}.yml`);
+        this.variables = variables;
+    }
+
+    async install() {
+        var cmd = `latex${this.version}.yml`;
+        await Baker.runAnsiblePlaybook({ name: this.name }, cmd, this.ansibleSSHConfig, this.verbose, this.variables);
+    }
+}
+
+module.exports = Latex;
