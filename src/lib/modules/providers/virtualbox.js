2cd9b1e8520b5f2a5bdd463497139e83bcde329d
Chris Parnin <chris.parnin@gmail.com>
2018-07-21 17:29:02 -0400

    Remove unneeded content for vbox driver.

--- src/lib/modules/providers/virtualbox.js
+++ src/lib/modules/providers/virtualbox.js
@@ -30,63 +30,10 @@ class VirtualBoxProvider extends Provider {
         this.driver = new VBoxProvider();
     }
 
-    async initVagrantFile(vagrantFilePath, doc, template, scriptPath) {
-        if (doc.vm ) {
-            doc.vagrant = doc.vm;
-            delete doc.vm;
-        }
-        const vagrant = doc.vagrant;
-        await Utils.traverse(vagrant);
-        // Defaults
-        // vagrant.box = vagrant.box || "ubuntu/xenial64"
-        // TODO: Cleanup this mess
-        if (vagrant.box && (await this.boxes()).map(e=>e.name).includes(`${vagrant.box}.baker`)){
-            vagrant.box = vagrant.box + '.baker';
-        }
-        else if(vagrant.box && (await this.boxes()).map(e=>e.name).includes(`${vagrant.box}`)){
-            vagrant.box = vagrant.box;
-        }
-        else{
-            vagrant.box = "ubuntu/xenial64";
-        }
-        vagrant.memory = vagrant.memory || "1024"
-
-        // Adaptor pattern: Support baker2 and baker format
-        let network = doc.vagrant.network || [];
-        if( vagrant.ip )
-        {
-            network = [...network, ...[{private_network: {ip: vagrant.ip}}]];
-        }
-        if( vagrant.ports )
-        {
-            // ports: '8000, 9000,  1000:3000'
-            let ports = vagrant.ports.toString().trim().split(/\s*,\s*/g);
-            for( var port of ports  )
-            {
-                let a = port.trim().split(/\s*:\s*/g);
-                let guest = a[0];
-                let host  = a[1] || a[0]; // if undefined use same as guest port for host port.
-                network = [...network, ...[{forwarded_port: {guest: guest, host: host}}]];
-            }
-        }
-        vagrant.network = network;
-
-        let syncFolders = doc.vagrant.synced_folders || [];
-        doc.vagrant.synced_folders = [...syncFolders, ...[{folder : {src: slash(scriptPath), dest: `/${path.basename(scriptPath)}`}}]];
-        const output = mustache.render(template, doc);
-        await fs.writeFile(vagrantFilePath, output);
-    }
-
     /**
      * Prune
      */
     static async prune() {
-        try {
-            await vagrant.globalStatusAsync('--prune');
-            return;
-        } catch (err) {
-            throw err;
-        }
     }
 
     async list() {
@@ -107,16 +54,7 @@ class VirtualBoxProvider extends Provider {
      * @param {boolean} verbose
      */
     async start(VMName, verbose = false) {
-        let machine = await this.getMachine(VMName);
-        machine.on('up-progress', function (data) {
-            if (verbose) print.info(data);
-        });
-
-        try {
-            await machine.upAsync();
-        } catch (err) {
-            throw `Failed to start machine ${VMName}`;
-        }
+        await vbox({start: true, vmname: doc.name, syncs: [], verbose: true});
     }
 
     /**
@@ -125,17 +63,7 @@ class VirtualBoxProvider extends Provider {
      * TODO: add force option
      */
     async stop(VMName, force = false) {
-        try {
-            let machine = await this.getMachine(VMName);
-            try {
-                await machine.haltAsync();
-            } catch (err) {
-                throw `Failed to shutdown machine ${VMName}`;
-            }
-        } catch (err) {
-            throw err;
-        }
-        return;
+        await vbox({stopCmd: true, vmname: doc.name, syncs: [], verbose: true});
     }
 
     /**
@@ -143,14 +71,7 @@ class VirtualBoxProvider extends Provider {
      * @param {String} VMName
      */
     async delete(VMName) {
-        let machine = await this.getMachine(VMName);
-
-        try {
-            await machine.destroyAsync();
-            Utils.removeFromIndex(VMName);
-        } catch (err) {
-            throw `Failed to destroy machine ${VMName}`;
-        }
+        await vbox({deleteCmd: true, vmname: doc.name, syncs: [], verbose: true});
     }
 
     /**
@@ -171,52 +92,12 @@ class VirtualBoxProvider extends Provider {
     }
 
     /**
-     * Private function
-     * Returns the path of the VM, undefined if VM doesn't exist
-     * @param {String} VMName
-     */
-    async getVMPath(VMName) {
-        let VMs = await vagrant.globalStatusAsync();
-        let VM = VMs.find(VM => VM.name === VMName);
-
-        if (VM)
-            return VM.cwd;
-        else
-            throw `Cannot find machine: ${VMName}`;
-    }
-
-    async getMachine(VMName) {
-        let VMPath = await this.getVMPath(VMName);
-        return vagrant.create({ cwd: VMPath });
-    }
-
-    /**
-     * Returns vagrant id of VMs by name
-     * @param {String} VMName
-     */
-    async getVagrantID(VMName) {
-        let VMs = await vagrant.globalStatusAsync();
-        let VM = VMs.find(VM => VM.name == VMName);
-
-        if (!VM)
-            throw `Cannot find machine: ${VMName}`;
-        return VM.id;
-    }
-
-    /**
      * Returns State of a VM
      * @param {String} VMName
      */
     static async getState(VMName) {
-        try {
-            let VMs = await vagrant.globalStatusAsync();
-            let VM = VMs.find(VM => VM.name == VMName);
-            if(!VM)
-                throw  `Cannot find machine: ${VMName}`;
-            return VM.state;
-        } catch (err) {
-            throw err;
-        }
+        let vmInfo = await this.driver.info(doc.name);
+        return vmInfo.VMState.replace('"','');
     }
 
     /**
@@ -323,90 +204,8 @@ class VirtualBoxProvider extends Provider {
     }
 
     static async retrieveSSHConfigByName(name) {
-        let dir = path.join(boxes, name);
-        let vm = vagrant.create({ cwd: dir });
-        let vmSSHConfigUser = await this.getSSHConfig(vm);
+        let vmSSHConfigUser = await this.getSSHConfig(name);
         return vmSSHConfigUser;
-    }
-
-
-    static async package(VMName, verbose) {
-        let dir = path.join(boxes, VMName);
-        await child_process.execAsync(`cd ${dir} && vagrant package --output ${path.join(process.cwd(), VMName + '.box')}`, {
-            stdio: ['inherit', 'inherit', 'ignore']
-        });
-    }
-
-    static async import(box, name, verbose) {
-        let boxName = name ? name : path.basename(box).split('.')[0];
-        await vagrant.boxAddAsync(path.join(process.cwd(), box), ['--name', boxName + '.baker'])
-        // await child_process.execAsync(`vagrant box add ${boxName}.baker ${path.join(process.cwd(), box)}`, {stdio: ['inherit', 'inherit', 'ignore']});
-    }
-
-    async boxes() {
-        try {
-            let boxes = await vagrant.boxListAsync([]);
-            delete boxes.version;
-            return boxes;
-        } catch (err) {
-            throw err
-        }
-    }
-
-    async bakerBoxes(verbose=true) {
-        try {
-            let boxes = await this.boxes();
-            let bakerBoxes = boxes.filter(box => box.name.match(/.baker$/));
-            // Hide .baker from the end before printing
-            bakerBoxes.forEach(box => {
-                box.name = box.name.split('.')[0];
-            })
-
-            if(verbose){
-                if(bakerBoxes == [])
-                    print.info(`\nYou currently don't have any boxes.`)
-                else
-                    console.table('\nBaker boxes: ', bakerBoxes);
-            }
-            return bakerBoxes;
-        } catch (err) {
-            throw err
-        }
-    }
-
-    async bakeBox(ansibleSSHConfig, ansibleVM, scriptPath, verbose) {
-        try {
-            let doc = yaml.safeLoad(await fs.readFile(path.join(scriptPath, 'baker.yml'), 'utf8'));
-
-            let dir = path.join(boxes, doc.name);
-            try {
-                await fs.ensureDir(dir);
-            } catch (err) {
-                throw `Creating directory failed: ${dir}`;
-            }
-
-            let template = await fs.readFile(path.join(configPath, './BaseVM.mustache'), 'utf8');
-
-            // if box is specified in baker.yml and this box exists, then use it => otherwise bake it
-            if(doc.vagrant.box && (await this.bakerBoxes(false)).map(e=>e.name).includes(`${doc.vagrant.box}`)){
-
-                await this.provider.initVagrantFile(path.join(dir, 'Vagrantfile'), doc, template, scriptPath);
-
-                let machine = vagrant.create({ cwd: dir });
-                machine.on('up-progress', function(data) {
-                    if( verbose ) print.info(data);
-                });
-                await spinner.spinPromise(machine.upAsync(), `Starting VM`, spinnerDot);
-            }
-            else {
-                await this.bakeBox(sshConfig, ansibleVM, bakePath, verbose);
-            }
-
-        } catch (err) {
-            throw err;
-        }
-
-        return;
     }
 }
 
